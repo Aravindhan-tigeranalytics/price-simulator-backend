11a9d898acbe34b0f7601ae6e5f4127b876e7ef6
Aravindhan Mathiyazhagan <aravindhan.mathi@tigeranalytics.local>
2021-09-01 12:35:25 +0530

    oprimizer calcultion changes

--- utils/models.py
+++ utils/models.py
@@ -71,7 +71,7 @@ class UnitModel:
         self.simulate_predicted_units = self.base_unit * (((1 - ((promo_depth + co_investment)/100))** decimal.Decimal(promo_elasticity)))
         # print()
         self.predicted_units = self.simulate_predicted_units if promo_elasticity else predicted_units
-        self.incremental_unit = round(self.simulate_predicted_units,5)  - round(self.base_unit,5)
+        self.incremental_unit = round(self.predicted_units,5)  - round(self.base_unit,5)
         self.lift = (self.incremental_unit / self.base_unit )
         # inc = new_pre - base
         self.asp =  decimal.Decimal(math.exp(median_base_price_log)) * decimal.Decimal(
